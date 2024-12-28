678adb8d527bd66c0fd0792093ff6dc25758bcb0
CristianoSFMothe <cristianodevsystem@gmail.com>
2024-12-28 01:38:20 -0300

    bugfix: initial integration with Hygraph

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,42 @@ import { AboutMe } from "./components/pages/about/about-me";
 import { HeroSection } from "./components/pages/home/hero-section";
 import { KnownTechs } from "./components/pages/known-techs";
 import { WorkExperience } from "./components/pages/work-experience";
+import { fetchHygraphQuery } from "./utils/fetch-hygraph-query";
+
+const getPageData = async () => {
+  const query = `
+  query PageInfoQuery {
+    page(where: {slug: "home"}) {
+      introduction {
+        raw
+      }
+      technologies {
+        name
+      }
+      profilePicture {
+        url
+      }
+      socials {
+        url
+        iconSvg
+      }
+      knownTechs {
+        iconSvg
+        name
+        startDate
+      }
+    }    
+  }
+`;
+
+  return fetchHygraphQuery(query);
+};
 
 export default async function Home() {
+  const response = await getPageData();
+
+  console.log(response);
+
   return (
     <>
       <HeroSection />
@@ -13,5 +47,5 @@ export default async function Home() {
       <HighlightedProjects />
       <WorkExperience />
     </>
-  )
+  );
 }
