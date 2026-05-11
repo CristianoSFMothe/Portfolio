b7c6e2d96358b412e43676e693707830c342c56e
Cristiano da Silva Ferreira <cristianodevsystem@gmail.com>
2026-05-11 07:13:40 -0300

    chore: recreate portfolio with updated Next.js setup

--- README.md
+++ README.md
@@ -1,35 +1,36 @@
-# 📂 Portfolio
+This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
+## Getting Started
 
-Este é um portfólio pessoal desenvolvido para apresentar meus principais projetos e minha experiência profissional. O design foi pensado de forma minimalista, mas com uma visualização detalhada de cada projeto, para que os visitantes possam compreender melhor as tecnologias e as soluções que utilizei em cada um deles. O portfólio também contém uma breve descrição de minha trajetória profissional e as principais habilidades adquiridas ao longo dos anos.
+First, run the development server:
 
-![image](https://github.com/user-attachments/assets/c839ae6a-868d-4e4b-b6f1-ae95134dc6da)
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
 
-### 🛠 Tecnologias Utilizadas:
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-- **Next.js** ![Next.js](https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white)
-- **TypeScript** ![TypeScript](https://img.shields.io/badge/TypeScript-3178C6?style=for-the-badge&logo=typescript&logoColor=white)
-- **Hygraph** ![Hygraph](https://img.shields.io/badge/Hygraph-FF00A6?style=for-the-badge&logo=hygraph&logoColor=white)
-- **Vercel** ![Vercel](https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white)
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-### 🎬 Animação:
+This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
-- **Framer Motion** ![Framer Motion](https://img.shields.io/badge/Framer%20Motion-00D084?style=for-the-badge&logo=framer&logoColor=white)
+## Learn More
 
-## 🚀 Como Acessar o Projeto
+To learn more about Next.js, take a look at the following resources:
 
-Você pode visualizar o portfólio no seguinte link:
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
-[Portfolio - Cristiano](https://portfolio-qa-cristiano.vercel.app/)
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
 
-## 📄 Visão Geral do Projeto
+## Deploy on Vercel
 
-O portfólio foi desenvolvido com o objetivo de apresentar de forma clara e visualmente atraente as principais realizações e projetos nos quais trabalhei. O design minimalista permite que o foco esteja nos projetos e na minha experiência profissional. Cada projeto possui uma descrição detalhada, destacando as tecnologias utilizadas e os desafios enfrentados durante o desenvolvimento.
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
-## 🤝 Contribuições
-
-Se você desejar contribuir com o projeto, fique à vontade para abrir um pull request ou sugerir melhorias através de issues. Será um prazer discutir novas ideias e implementações!
-
-## 📜 Licença
-
-Este projeto está sob a licença MIT - veja o arquivo [LICENSE](./LICENSE) para mais detalhes.
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
--- app/page.tsx
+++ app/page.tsx
@@ -1,89 +1,65 @@
-import { HighlightedProjects } from "./components/pages/highlighted-projects";
-import { HeroSection } from "./components/pages/home/hero-section";
-import { KnownTechs } from "./components/pages/known-techs";
-import { WorkExperience } from "./components/pages/work-experience";
-import { fetchHygraphQuery } from "./utils/fetch-hygraph-query";
-import { HomePageData } from "./types/page-info";
-import { AboutMe } from "./components/pages/about";
+import Image from "next/image";
 
-const getPageData = async (): Promise<HomePageData> => {
-  const query = `
-  query PageInfoQuery {
-    page(where: {slug: "home"}) {
-      introduction {
-        raw
-      }
-      technologies {
-        name
-      }
-      profilePicture {
-        url
-      }
-      socials {
-        url
-        iconSvg
-      }
-      knownTechs {
-        iconSvg
-        name
-        startDate
-      }
-      highlightProjects {
-        slug
-        thumbnail {
-          url
-        }
-        title
-        shortDescription
-        technologies {
-          name
-        }
-      }
-      profileAbout {
-        url
-      }
-      urlCurriculum
-      aboutMe {
-        raw
-      }
-    }
-    workExperiences {
-      companyLogo {
-        url
-      }
-      role
-      companyName
-      companyUrl
-      startDate
-      endDate
-      description {
-        raw
-      }
-      technologies {
-        name
-      }
-    }
-  }
-`;
-
-  return fetchHygraphQuery(
-    query,
-    60 * 60 * 1, // 1 hora
-  );
-};
-
-export default async function Home() {
-  const { page: pageData, workExperiences } = await getPageData();
-
-  // console.log(pageData);
-
+export default function Home() {
   return (
-    <>
-      <HeroSection homeInfo={pageData} />
-      <KnownTechs techs={pageData.knownTechs} />
-      <AboutMe homeInfo={pageData} />
-      <HighlightedProjects projects={pageData.highlightProjects} />
-      <WorkExperience experiences={workExperiences} />
-    </>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
   );
 }
